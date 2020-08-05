7892e20a7da6c1629b3421d3bbbbb09c1e6da400
Maria Zinchyn <52235216+mariazinchyn@users.noreply.github.com>
2020-08-05 16:51:37 +0300

    Add files via upload

---
+++ springboot-crud-restful-webservisce/src/main/java/com/maria/SpringbootCrudRestfulWebservisceApplication.java
@@ -0,0 +1,13 @@
+package com.maria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudRestfulWebservisceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudRestfulWebservisceApplication.class, args);
+	}
+
+}
